57c88b85d64c3eb18109b1cb5b84b36b4b80297e
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-03-18 19:01:04 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { MyteamComponent } from './sharepages/myteam/myteam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    MyteamComponent
   ],
   imports: [
     BrowserModule,
